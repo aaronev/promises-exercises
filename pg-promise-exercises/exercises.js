13cc16e6515277a785688dd2a3ab0c8cba02889e
Aaron Villanueva <aaronvillanueva@Aarons-MacBook-Pro.local>
2017-04-04 15:47:41 -0700

    finished exercises

--- pg-promise-exercises/exercises.js
+++ pg-promise-exercises/exercises.js
@@ -1,30 +1,31 @@
+const assert = require('chai').assert
 const pgp = require('pg-promise')();
 const connectionString = 'postgres://localhost:5432/pg-promise-exercises';
 const db = pgp(connectionString);
 
 const allBooks = db.any('SELECT * FROM books')
-  allbooks.then(books => {
-    assert.deepEqual(books.length, 15)
-    assert.deepEqual(book[0].title, 'The Shining')
-    assert.deepEqual(book[1].title, 'Dune')
+  .then(books => {
+    assert.equal(books.length, 15)
+    assert.equal(books[0].title, 'The Shining')
+    assert.equal(books[1].title, 'Dune')
   }).catch(error => {
     console.log('Dang, my assertion failed.', error );
   });
 
 let firstTenBooks = db.any('SELECT * FROM books LIMIT 10')
-  firstTenBooks.then(books => {
-    assert.deepEqual(books.length, 10)
-    assert.deepEqual(books[3].subject_id, 2)
-    assert.deepEqual(books[4].author_id, 1809)
+  .then(books => {
+    assert.equal(books.length, 10)
+    assert.equal(books[3].subject_id, 2)
+    assert.equal(books[4].author_id, 1809)
   }).catch(error => {
     console.log('Whoops, my function doesnt behave as expected.', error);
   });
 
 let findAuthorsOrderedByLastName = db.any('SELECT * FROM authors ORDER BY last_name')
-  findAuthorsOrderedByLastName.then(authors => {
-    assert.deepEqual(authors.length, 21)
-    assert.deepEqual(authors[0].last_name, 'Alcott')
-    assert.deepEqual(authors[18].last_name, 'Poe')
+  .then(authors => {
+    assert.equal(authors.length, 21)
+    assert.equal(authors[0].last_name, 'Alcott')
+    assert.equal(authors[18].last_name, 'Simon')
   }).catch(error => {
     console.log('Whoops, my function doesnt behave as expected.', error);
   });
@@ -32,10 +33,10 @@ let findAuthorsOrderedByLastName = db.any('SELECT * FROM authors ORDER BY last_n
 let findBookAuthors = db.any(
   'SELECT first_name, last_name, title FROM authors JOIN books ON books.author_id = authors.id'
   )
-  findBookAuthros.then(authors => {
-    assert.deepEqual(books.length, 17)
-    assert.deepEqual(authors[0].first_name, 'John')
-    assert.deepEqual(authors[12].last_name, 'Christiansen')
+  .then(authors => {
+    assert.equal(authors.length, 17)
+    assert.equal(authors[0].first_name, 'John')
+    assert.equal(authors[12].last_name, 'Clarke')
   }).catch(error => {
     console.log('The data is just...like...in space...or something', error);
   });
@@ -43,10 +44,10 @@ let findBookAuthors = db.any(
 let authorIdWithTwoBooks = db.any(
   'SELECT author_id FROM books GROUP BY author_id HAVING COUNT(author_id) >= 2'
   )
-  authroIdWithTwoBooks.then(books => {
-    assert.deepEqual(books.length, 2)
-    assert.deepEqual(books[0].author_id, 1809)
-    assert.deepEqual(books[1].author_id, 7805)
+  .then(books => {
+    assert.equal(books.length, 2)
+    assert.equal(books[0].author_id, 1809)
+    assert.equal(books[1].author_id, 7805)
   }).catch(error => {
     console.log('The asserts did not work', error);
   });
@@ -54,10 +55,10 @@ let authorIdWithTwoBooks = db.any(
 let bookTitlesWithMultipleEditions = db.any(
   'SELECT title FROM editions JOIN books ON editions.book_id = books.id GROUP BY title HAVING COUNT(title) >= 2'
   )
-  bookTitlesWithMultipleEditions.then(editions => {
-    assert.deepEqual(editions.length, 5)
-    assert.deepEqual(editions[0].title, 'The Shining')
-    assert.deepEqual(editions[4].title, 'The Tell-Tale Heart')
+  .then(editions => {
+    assert.equal(editions.length, 5)
+    assert.equal(editions[0].title, 'The Shining')
+    assert.equal(editions[4].title, 'The Tell-Tale Heart')
   }).catch(error => {
     console.log('The asserts did not work', error);
   });
@@ -70,13 +71,13 @@ let findStockedBooks = db.any(
   JOIN authors ON books.author_id = authors.id \
   WHERE daily_inventory.is_stocked = 't'"
   )
-  findStockedBooks.then(daily_inventory => {
-    assert.deepEqual(daily_inventory.length, 2)
-    assert.deepEqual(daily_invetory[0].title, 'Dune')
-    assert.deepEqual(daily_inventory[1].last_name, 'Geisel')
+  .then(daily_inventory => {
+    assert.equal(daily_inventory.length, 2)
+    assert.equal(daily_inventory[0].title, 'Dune')
+    assert.equal(daily_inventory[1].last_name, 'Geisel')
   }).catch(error => {
     console.log('The asserts did not work', error);
   });
 
 console.log('Reached the end!');
-pg.end();
+pgp.end();
